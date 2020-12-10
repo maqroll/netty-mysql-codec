0f100f8c8ff508a59d09ce6e3087d7a3f620a7cf
maqroll <loteroc@gmail.com>
2020-12-10 17:16:34 +0100

    + visit(Ok|Error|Eof)

--- codec/src/main/java/com/github/mheath/netty/codec/mysql/MysqlServerPacketVisitor.java
+++ codec/src/main/java/com/github/mheath/netty/codec/mysql/MysqlServerPacketVisitor.java
@@ -3,5 +3,8 @@ package com.github.mheath.netty.codec.mysql;
 import io.netty.channel.ChannelHandlerContext;
 
 public interface MysqlServerPacketVisitor {
-  void visit(Handshake pkt, ChannelHandlerContext ctx);
+  void visit(Handshake handshake, ChannelHandlerContext ctx);
+  void visit(OkResponse ok, ChannelHandlerContext ctx);
+  void visit(EofResponse eof, ChannelHandlerContext ctx);
+  void visit(ErrorResponse error, ChannelHandlerContext ctx);
 }
