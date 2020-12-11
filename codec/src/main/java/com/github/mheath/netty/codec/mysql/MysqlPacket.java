0170527a324157c46f49807f4dad6fc63f29c3cc
maqroll <loteroc@gmail.com>
2020-12-11 13:35:50 +0100

    + Visitable

--- codec/src/main/java/com/github/mheath/netty/codec/mysql/MysqlPacket.java
+++ codec/src/main/java/com/github/mheath/netty/codec/mysql/MysqlPacket.java
@@ -18,11 +18,6 @@ package com.github.mheath.netty.codec.mysql;
 
 import io.netty.channel.ChannelHandlerContext;
 
-public interface MysqlPacket {
-
+public interface MysqlPacket extends Visitable {
 	int getSequenceId();
-
-	default void accept(MysqlServerPacketVisitor visitor, ChannelHandlerContext ctx) {
-		throw new IllegalStateException("Failed to accept");
-	}
 }
